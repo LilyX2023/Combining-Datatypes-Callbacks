c063e120fef8f5082a3113a6cba852fe201b486f
RachelY <yang.yijing@outlook.com>
2023-11-24 21:48:39 -0500

    Combine objects, arrays, and functions more than one level deep

--- combining_datatypes.js
+++ combining_datatypes.js
@@ -1,6 +1,8 @@
 const crayonBox = {
     colors: ["red", "blue", "green", "cyan", "purple"]
 }
+// Log one of the elements of the array
+console.log(crayonBox.colors[1])
 
 const bottle = {
     cap: {
@@ -8,6 +10,9 @@ const bottle = {
         color: 'blue'
     }
 }
+//Log one of the properties of the inner object cap
+console.log(bottle.cap.color);
+
 
 const receipt = [
     {
@@ -23,7 +28,42 @@ const receipt = [
         price: 3.00
     }
 ]
+//Log one of the properties of that inner object.
+console.log(receipt[1].name);
 
 const apartmentBuilding = [
     ['Olivia','Liam','Sophia','Noah']
 ]
+//Log one of the elements of the inner array
+console.log(apartmentBuilding[0][1])
+
+/////////////////////////////////////////////////////
+
+const knit = () => {
+    // Return an object with properties
+    return {
+        item: 'scarf',
+        size: '6ft'
+    };
+}
+
+// Log a value of the returned 
+console.log(knit().size)
+
+const crayonSelector = () => {
+    return {
+        colors: ["red", "blue", "green", "cyan", "purple"]
+    }
+}
+//Log one of the elements of that array.
+console.log(crayonSelector().colors[2])
+
+const powerButton = () => {
+    return option = () => {
+        console.log("Select a song.")
+    }
+}
+powerButton()()
+
+///////////////////////////////////////////////////
+
